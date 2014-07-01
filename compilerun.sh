a9df6e546bdbc760e23481134b8823fc80238c91
Michael Henke <smack42@googlemail.com>
2014-07-01 22:28:21 +0200

    update compilerun.sh to create Java 6 compatible classes

--- compilerun.sh
+++ compilerun.sh
@@ -8,7 +8,7 @@ SRCDIR=src
 echo compile...
 rm -rf $DESTDIR
 mkdir $DESTDIR
-javac -sourcepath $SRCDIR -d $DESTDIR -cp designgridlayout-1.11.jar $SRCDIR/driftingdroids/ui/*.java $SRCDIR/driftingdroids/model/*.java
+javac -sourcepath $SRCDIR -d $DESTDIR -cp designgridlayout-1.11.jar -source 1.6 -target 1.6 $SRCDIR/driftingdroids/ui/*.java $SRCDIR/driftingdroids/model/*.java
 cp -rp $SRCDIR/META-INF $SRCDIR/*.properties $DESTDIR
 
 echo
