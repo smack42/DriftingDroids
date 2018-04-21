8a49c44ee7b7b801f4041f520bc03bc794047e93
Michael Henke <smack42@gmail.com>
2018-04-21 17:34:08 +0200

    use ProGuard 6.0

--- buildjar.sh
+++ buildjar.sh
@@ -2,7 +2,7 @@ OUTPUT=lib/driftingdroids.jar
 
 rm -f $OUTPUT
 
-java -jar $HOME/Programme/proguard4.9/lib/proguard.jar @proguard_config -outjars $OUTPUT
+java -jar $HOME/Programme/proguard6.0/lib/proguard.jar @proguard_config -outjars $OUTPUT
 
 chmod u+x $OUTPUT
 
