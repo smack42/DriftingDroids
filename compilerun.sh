737be7840b78294a69ab26308b3b14e1095b6d8a
Michael Henke <smack42@gmail.com>
2021-12-12 15:44:29 +0100

    fix script compilerun.sh
    now compiles for Java 8 and references the "lib" folder.
    
    in response to issue #23

--- compilerun.sh
+++ compilerun.sh
@@ -1,17 +1,17 @@
-# To start DriftingDroids from the bash shell
-# run this script in the root folder of the repository with
-# ./run.sh
+# To start DriftingDroids from the bash shell,
+# run this script in the root folder of the repository
 
 DESTDIR=bin
 SRCDIR=src
+LIBDIR=lib
 
 echo compile...
 rm -rf $DESTDIR
 mkdir $DESTDIR
-javac -sourcepath $SRCDIR -d $DESTDIR -cp designgridlayout-1.11.jar -source 1.6 -target 1.6 $SRCDIR/driftingdroids/ui/*.java $SRCDIR/driftingdroids/model/*.java
+javac -sourcepath $SRCDIR -d $DESTDIR -cp $LIBDIR/designgridlayout-1.11.jar -source 1.8 -target 1.8 $SRCDIR/driftingdroids/ui/*.java $SRCDIR/driftingdroids/model/*.java
 cp -rp $SRCDIR/META-INF $SRCDIR/*.properties $DESTDIR
 
 echo
 echo run...
-java -Xmx800M -cp $DESTDIR:designgridlayout-1.11.jar driftingdroids.ui.Starter
+java -Xmx800M -cp $DESTDIR:$LIBDIR/designgridlayout-1.11.jar driftingdroids.ui.Starter
 
