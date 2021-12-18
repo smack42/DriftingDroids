a876ce3f04c6d0831e5dfe6c7d8208e53bf953ad
Michael Henke <smack42@gmail.com>
2021-12-18 13:26:11 +0100

    move Appstart files into lib folder
    Appstart - The Universal Java Application Launcher
    http://code.google.com/p/appstart/
    https://github.com/smack42/appstart
    
    in response to issue #23

--- buildrelease.sh
+++ buildrelease.sh
@@ -8,8 +8,8 @@ jar --create --file $DESTDIR/lib/driftingdroids.jar --no-compress --manifest src
 cp -p  lib/designgridlayout-1.11.jar  $DESTDIR/lib
 cp -p  lib/flatlaf*.jar               $DESTDIR/lib
 
-cp -p  appstart.properties            $DESTDIR
-cp -p  start.jar                      $DESTDIR
+cp -p  lib/appstart.properties        $DESTDIR
+cp -p  lib/appstart.jar               $DESTDIR/start.jar
 
 cp -p  CHANGES.txt                    $DESTDIR
 cp -p  LICENSE.txt                    $DESTDIR
